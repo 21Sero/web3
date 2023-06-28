615cf9c1356afb10a3458a0db41bf3f3090e7ef4
seroharutyunyan <seroharutyunyan@gmail.com>
2023-06-28 10:47:47 +0400

    initial commit

---
+++ send/src/container/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { images } from "../../constants"
+import "./header.css"
+import Subheading from "../../components/SubHeding/Subheading";
+const Header = () => {
+
+    return (
+        <div className="app__header app__wrapper section__padding " id="home">
+            <div className="app__wrapper_info">
+
+                <h1 className="app__header-h1"> SEA & Food  </h1>
+                <p className="p__opensans" style={{ margin: "2rem 0"}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+                <button type="button" className="custom__button"> Explore Menu </button>
+           </div>
+
+            <div className="app__wrapper_img">
+                <img src={images.welcome} alt="header_img"/>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Footer,Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
+import { Cta, Navbar, Brand } from "./components";
+import "./App.css"
+import "./index.css"
+function App() {
+  return (
+    <div className="App">
+     <div className="gradient__bg">
+         <Navbar />
+         <Header />
+     </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
